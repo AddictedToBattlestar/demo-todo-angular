e8f06a5d96829af2839cd935b83352e5ba5e6621
AddictedToBattlestar <addictedtobsg@gmail.com>
2023-02-16 22:11:00 -0500

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListingComponent } from './todo-listing/todo-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListingComponent
   ],
   imports: [
     BrowserModule,
